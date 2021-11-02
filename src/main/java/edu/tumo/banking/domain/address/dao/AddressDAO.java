07338de30dfc85898e113ed877b36f9b4c07a504
Laura <63228952+laurabaldrayan@users.noreply.github.com>
2021-11-02 15:27:15 -0700

    Interfaces

--- src/main/java/edu/tumo/banking/domain/address/dao/AddressDAO.java
+++ src/main/java/edu/tumo/banking/domain/address/dao/AddressDAO.java
@@ -1,5 +1,37 @@
 package edu.tumo.banking.domain.address.dao;
 
-public class AddressDAO {
+import edu.tumo.banking.domain.address.dto.AddressDTO;
+import edu.tumo.banking.domain.address.model.AddressModel;
+import edu.tumo.banking.repository.address.AddressRepository;
+import org.springframework.data.annotation.Id;
 
+import java.util.List;
+import java.util.Optional;
+
+public abstract class AddressDAO implements AddressRepository<AddressModel,Id> {
+
+    @Override
+    public List<AddressDTO> findall() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO add(AddressDTO address) {
+        return null;
+    }
+
+    @Override
+    public Optional<AddressDTO> findBy(Id i) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteAddressDTOBy(Id i) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
--- src/main/java/edu/tumo/banking/repository/address/AddressRepository.java
+++ src/main/java/edu/tumo/banking/repository/address/AddressRepository.java
@@ -1,4 +1,16 @@
 package edu.tumo.banking.repository.address;
 
-public interface AddressRepository {
+import edu.tumo.banking.domain.address.dto.AddressDTO;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AddressRepository<A, I> extends CrudRepository<AddressDTO, Id> {
+    List<AddressDTO> findall();
+    AddressDTO add(AddressDTO address);
+    Optional<AddressDTO> findBy(Id i);
+    void deleteAddressDTOBy(Id i);
+    void deleteAll();
 }
