870e8fa39d7f8af1ab6c31adbb559aa0c8e94818
YekaterinaAvagimyan <avagimyancathy@gmail.com>
2021-11-30 20:18:54 +0400

    employeeservice

--- src/main/java/edu/tumo/banking/service/employee/EmployeeServiceImpl.java
+++ src/main/java/edu/tumo/banking/service/employee/EmployeeServiceImpl.java
@@ -1,17 +1,33 @@
 package edu.tumo.banking.service.employee;
 
 
+import edu.tumo.banking.domain.employee.model.EmployeeModel;
 import edu.tumo.banking.repository.employee.EmployeeRepository;
+import edu.tumo.banking.repository.employee.EmployeeRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeServiceImpl {
-//
-//    private final EmployeeRepository employeeRepository;
-//
-//    @Autowired
-//    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-//        this.employeeRepository = employeeRepository;
-//    }
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<EmployeeModel> findEmployees() {return EmployeeRepositoryImpl.findAll();}
+
+    public EmployeeModel addEmployee(EmployeeModel employee) {return EmployeeRepositoryImpl.add(employee);}
+
+    public Optional<EmployeeModel> findEmployeeById(Long id) {return EmployeeRepositoryImpl.findById(id);}
+
+    public Optional<EmployeeModel> updateEmployee(EmployeeModel employee) {return  EmployeeRepositoryImpl.update(employee);}
+
+    public void deleteEmployeeById(Long id) {EmployeeRepositoryImpl.deleteEmployeeModelById(id);}
+
 }
--- src/main/java/edu/tumo/banking/service/user/UserService.java
+++
@@ -1,4 +0,0 @@
-package edu.tumo.banking.service.user;
-
-public interface UserService {
-}
