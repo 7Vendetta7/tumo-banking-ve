37052a787b89fc24f38c7a12069583c07e663606
YekaterinaAvagimyan <avagimyancathy@gmail.com>
2021-11-30 20:29:52 +0400

    status

--- src/main/java/edu/tumo/banking/database/creationOfTables.sql
+++ src/main/java/edu/tumo/banking/database/creationOfTables.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS employee(
     employee_id BIGINT PRIMARY KEY auto_increment,first_name VARCHAR(255) NOT NULL UNIQUE,
     last_name VARCHAR(255) NOT NULL UNIQUE, age INT NOT NULL UNIQUE, salary FLOAT NOT NULL UNIQUE,
     address VARCHAR(255) NOT NULL UNIQUE,department VARCHAR(255) NOT NULL UNIQUE,
-    employees_statue BIT NOT NULL UNIQUE,
+    employees_status BIT NOT NULL UNIQUE,
     bank_id BIGINT NOT NULL UNIQUE,
     FOREIGN KEY (bank_id) REFERENCES bank(bank_id));
 
