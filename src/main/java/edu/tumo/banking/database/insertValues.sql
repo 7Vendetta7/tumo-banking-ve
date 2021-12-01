3a2490274e08f3ccacf7fc90e0c589af16670062
Laura <63228952+laurabaldrayan@users.noreply.github.com>
2021-12-01 11:29:31 -0800

    sql inserts

---
+++ src/main/java/edu/tumo/banking/database/insertValues.sql
@@ -0,0 +1,5 @@
+insert into user values(null,'login','password');
+
+insert into bank values(null,'Acba Credit Agricole Bank','35/1 Arshakunyac Street, Yerevan 0026');
+
+insert into employee values(null,'Laura','Baldryan','21',223000.0,'Smbat Zoravar 48/1','IT',true ,1);
