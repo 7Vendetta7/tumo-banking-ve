291008dd529fb648e352f5cace4bef7e34a906d6
Laura <63228952+laurabaldrayan@users.noreply.github.com>
2021-12-02 14:30:07 -0800

    hikari-test

---
+++ src/main/java/edu/tumo/banking/configuration/HikariConfig.java
@@ -0,0 +1,14 @@
+package edu.tumo.banking.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HikariConfig {
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Test
+//    public void hikariConnectionPoolIsConfigured() {
+//        assertEquals("com.zaxxer.hikari.HikariDataSource", dataSource.getClass().getName());
+}
