2384257d2190ed1d7586780bb9409d3b1028b3e7
YekaterinaAvagimyan <avagimyancathy@gmail.com>
2021-12-03 02:30:12 +0400

    user validaton user controller

--- src/main/java/edu/tumo/banking/controller/UserController.java
+++ src/main/java/edu/tumo/banking/controller/UserController.java
@@ -1,6 +1,8 @@
 package edu.tumo.banking.controller;
 
+import edu.tumo.banking.domain.employee.model.EmployeeModel;
 import edu.tumo.banking.domain.user.UserModel;
+import edu.tumo.banking.repository.validation.UserValidation;
 import edu.tumo.banking.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,18 +10,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+    private final UserValidation userValidation;
 
     @Autowired
-    public UserController(UserService userService) {this.userService = userService;}
+    public UserController(UserService userService, UserValidation userValidation) {
+        this.userService = userService;
+        this.userValidation = userValidation;
+    }
 
     @PostMapping
     public ResponseEntity<UserModel> adduser(@RequestBody UserModel user){
+        if(!(userValidation.validateForNull(user)))
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(userService.add(user), HttpStatus.CREATED);
     }
 
@@ -31,11 +42,16 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserModel> findUserById(@PathVariable Long id){
-        return new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
+        Optional<UserModel> model=userService.findById(id);
+        return new ResponseEntity<>(model.get(), HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<UserModel> updateUser(@RequestBody UserModel updatedUser){
+        if(!(userValidation.validateForNull(updatedUser)))
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(userService.update(updatedUser), HttpStatus.OK);
     }
 
