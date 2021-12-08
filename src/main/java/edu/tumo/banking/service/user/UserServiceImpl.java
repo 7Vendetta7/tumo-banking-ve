66b7388628c359b5c596343864b9c996adefddff
YekaterinaAvagimyan <avagimyancathy@gmail.com>
2021-12-08 04:45:57 +0400

    yesim inch

--- src/main/java/edu/tumo/banking/service/user/UserServiceImpl.java
+++ src/main/java/edu/tumo/banking/service/user/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package edu.tumo.banking.service.user;
 
 import edu.tumo.banking.domain.user.UserModel;
+import edu.tumo.banking.exception.ResourceNotFound;
 import edu.tumo.banking.repository.user.UserRepositoryImp;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl  implements UserService{
+
+    private final Logger logger = LogManager.getLogger(UserServiceImpl.class);
 
     private final UserRepositoryImp userRepositoryImp;
 
@@ -18,27 +24,47 @@ public class UserServiceImpl  implements UserService{
     }
 
     @Override
+    @Transactional
     public List<UserModel> findAll() {
+
+        logger.info("The users are found");
         return userRepositoryImp.findAll();
     }
 
     @Override
+    @Transactional
     public UserModel add(UserModel userModel) {
+
+
+
         return userRepositoryImp.add(userModel);
     }
 
     @Override
+    @Transactional
     public UserModel update(UserModel userModel) {
+
         return userRepositoryImp.update(userModel);
     }
 
     @Override
+    @Transactional
     public UserModel findById(Long id) {
+        UserModel userModel = userRepositoryImp.findById(id).orElse(null);
+        if(userModel == null) {
+            logger.warn("user {} is not found", id);
+          //  Throwable e =  "exception of user not found with id " +
+          //  throw new ResourceNotFound("ssjhdj"+ e);
+
+        }
         return userRepositoryImp.findById(id).get();
     }
 
+    @Override
+    @Transactional
     public void deleteUserById(Long id)
     {
+
         userRepositoryImp.deleteUserById(id);
     }
 
